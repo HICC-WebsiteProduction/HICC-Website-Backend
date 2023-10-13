c07230df648fbf26863d27e5c023099790cfa600
leesh <qwqw8019@naver.com>
2023-10-13 19:30:38 +0900

    environment settings commit

---
+++ src/main/java/HICC/HICCWebsiteBackend/HiccWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package HICC.HICCWebsiteBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiccWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiccWebsiteBackendApplication.class, args);
+	}
+
+}
